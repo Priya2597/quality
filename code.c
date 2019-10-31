eb63d31605830395ee80c70c776c9dcecffaae72
Priya2597 <57185541+Priya2597@users.noreply.github.com>
2019-10-31 09:43:02 +0530

    Update code.c

--- code.c
+++ code.c
@@ -2,11 +2,11 @@
  
 int main()
 {
-  int array[100], c, d, swap;
+  int array[100], n,c,e, d, swap;
   int i;
  
   printf("Enter number of elements\n");
-  scanf("%u", &n);
+  scanf("%d", &n);
  
   printf("Enter %d integers\n", n);
  
@@ -17,7 +17,7 @@ int main()
   {
     for (d = 0 ; d < n - c - 1; d++)
     {
-      if (array[d] > array[d+1]) /* For decreasing order use < */
+      if (array[d] > array[d+1])
       {
         swap       = array[d];
         array[d]   = array[d+1];
